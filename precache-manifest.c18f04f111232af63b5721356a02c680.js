364980dbf246364bf5ba805a7140a37e0237f6fa
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-08-30 18:36:27 -0400

    Updates

---
+++ precache-manifest.c18f04f111232af63b5721356a02c680.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1597834f01e0381a5bc6f1eca44a25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84a4c292be77d4837507",
+    "url": "/static/css/main.5949c584.chunk.css"
+  },
+  {
+    "revision": "6e9de45fea165427b3f0",
+    "url": "/static/js/2.ff9d3111.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff9d3111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a4c292be77d4837507",
+    "url": "/static/js/main.f596f437.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "4e670fb0a18e98d88a8572c2f2cce831",
+    "url": "/static/media/GitGud.4e670fb0.jpg"
+  }
+]);
